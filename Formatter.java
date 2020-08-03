803751751a865a4002fb6bcffdaa15b174796f8c
ydylwj23 <35717915+ydylwj23@users.noreply.github.com>
2020-08-02 22:22:25 -0700

    add 4

--- Formatter.java
+++ Formatter.java
@@ -1,44 +1,55 @@
-class Solution {
-    public boolean validUtf8(int[] data) {
-        for (int i = 0; i < data.length;) {
-            int cur = checkCode(data, i);
-            if (cur == -1) {
-                return false;
-            }
-            i += cur;
-        }
-        return true;
-    }
+import java.util.*;
 
-    private int checkCode(int[] data, int index) {
-        int head = data[index];
-        int checkBit = 1 << 7;
-        int codeLength = 0;
-        // find code byte length
-        if ((head & checkBit) == 0) {
-            return 1;
-        } else if ((head & (checkBit >> 1)) == 0) {
-            return -1;
-        } else if ((head & (checkBit >> 2)) == 0) {
-            codeLength = 2;
-        } else if ((head & (checkBit >> 3)) == 0) {
-            codeLength = 3;
-        } else if ((head & (checkBit >> 4)) == 0) {
-            codeLength = 4;
-        } else {
-            return -1;
-        }
-        // check trailing bytes
-        for (int i = index + 1; i < index + codeLength; ++i) {
-            // if out of bound
-            if (i >= data.length) {
-                return -1;
-            }
-            int curByte = data[i];
-            if ((checkBit & curByte) == 0 || ((checkBit >> 1) & curByte) != 0) {
-                return -1;
-            }
-        }
-        return codeLength;
+class Main {
+  static public void main( String args[] ) {
+    System.out.println( "Practice makes Perfect!" );
+    int[] arr = new int[]{3,2,1,5,6,4};
+    int result = kLargestElement(arr, 2);
+    System.out.println( result );
+    
+  }
+
+  static public int kLargestElement(int[] arr, int k) {
+    int n = arr.length;
+    return partition(arr, 0 , n - 1, n, k);
+  }
+  static private int partition(int[] arr, int start, int end, int n, int k) {
+    //base case
+    if (end < 0) {
+      return arr[start];
     }
+    if (start >= n) {
+      return arr[end];
+    }
+    if (start == end) {
+      return arr[start];
+    }
+    //take end as pivot
+    int pivot = arr[end];
+    int i = start;
+    int j = start;
+    while (j < end){
+      if (arr[j] < pivot) {
+        swap(arr,i, j);
+        ++i;
+      } 
+      ++j;
+    }
+    swap(arr, i, end);
+    if (i == n - k) {//find the kth largest
+      return arr[i];
+    } else if (i < n - k) {
+      return partition(arr, i + 1, end, n , k);
+    } else {// i > n - k
+      return partition(arr, start, i - 1, n , k);
+      
+      
+    }
+  }
+  static private void swap(int[] arr, int i, int j) {
+    int t = arr[i];
+    arr[i] = arr[j];
+    arr[j] =t;
+    
+  }
 }
