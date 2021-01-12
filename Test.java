4a27cac8735bfc52fb18126c9de0dab542635b03
Jue Wang <ydylydyl@gmail.com>
2021-01-11 21:13:02 -0800

    add debug file

---
+++ Test.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Test {
+    public static void main(String[] args) {
+        Test test = new Test();
+        int res = test.twoSum(5, 6);
+    }
+
+    public int twoSum(int a, int b) {
+        return a + b;
+    }
+}
